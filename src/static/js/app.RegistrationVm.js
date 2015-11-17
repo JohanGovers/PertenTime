06405671fcf64eec40d8ec107129adfdbe66eab0
JohanGovers <att.johgo@gmail.com>
2015-11-17 20:30:55 +0100

    Issue #44 display favourites on top and mark them with a star.

--- src/static/js/app.RegistrationVm.js
+++ src/static/js/app.RegistrationVm.js
@@ -2,36 +2,36 @@
 
 function TimeEntry(projectId, date, hours, submitted, vm) {
 	var self = this;
-	
+
 	self.projectId = projectId;
 	self.date = date;
 	self.hasError = ko.observable(false);
 	self.hours = ko.observable(hours).extend({ rateLimit: { timeout: 400, method: "notifyWhenChangesStop" } });
 	self.submitted = ko.observable(submitted);
-	
+
 	self.hours.subscribe(function(){
 		//Sanitize input of time.
 		var value = self.hours();
-		
+
 		var value = self.hours().replace(',', '.');
-	
+
 		//No non numeric characters.
 		value = value.replace(/[^\d.$]/g, '');
-		
+
 		//Only one decimal point
 		if ((value.match(/\./g) || []).length > 1) {
-			var idxFirstDot = value.indexOf('.');  
+			var idxFirstDot = value.indexOf('.');
 			var idxSecondDot = value.indexOf('.', idxFirstDot + 1);
 			value = value.substring(0, idxSecondDot);
 		}
-		
+
 		//Allow only 2 decimals
 		if ((value.split('.')[1] || []).length > 2){
 			value = Number(value).toFixed(2);
 		}
-		
+
 		self.hours(value);
-			
+
 		$.ajax({
 			type: 'POST',
 			url: 'save_time_entry',
@@ -48,13 +48,14 @@ function TimeEntry(projectId, date, hours, submitted, vm) {
 	});
 }
 
-function Project(id, code, name, timeentries, vm) {
+function Project(id, code, name, favourite, timeentries, vm) {
 	var self = this;
-	
+
 	self.id = id;
 	self.code = code;
 	self.name = name;
-	
+	self.favourite = favourite;
+
 	self.timeentries = ko.observableArray(timeentries.map(function(entry){
 		return new TimeEntry(id, entry.date, entry.hours, entry.submitted, vm);
 	}));
@@ -62,7 +63,7 @@ function Project(id, code, name, timeentries, vm) {
 
 function RegistrationVm() {
 	var self = this;
-	
+
 	self.savedAt = ko.observable();
 	self.dataLoadingInProgress = ko.observable(false);
 	self.showErrorMessage = ko.observable(false);
@@ -70,14 +71,24 @@ function RegistrationVm() {
 		console.error(status, error, "Something went wrong when submitting.", self);
 		self.showErrorMessage(true);
 	};
-	
+
 	self.startDate = ko.observable();
 	self.endDate = ko.observable();
 	self.submittedUntil = ko.observable();
 	self.projects = ko.observableArray();
-	
+	self.favouriteProjects = ko.pureComputed(function(){
+		var favs = [];
+		for (var i = 0; i < self.projects().length; i++){
+			if(self.projects()[i].favourite){
+				favs.push(self.projects()[i])
+			}
+		}
+		return favs;
+	});
+
+
 	self.allSubmitted = ko.observable(false);
-	
+
 	self.totalHours = ko.computed(function(){
 		var sum = 0;
 		var projects = self.projects();
@@ -90,13 +101,13 @@ function RegistrationVm() {
 				}
 			}
 		}
-		
+
 		return sum;
 	});
 	self.disableSubmitButton = ko.computed(function(){
 		return self.dataLoadingInProgress() || self.endDate() <= self.submittedUntil();
 	});
-	
+
 	self.loadData = function(){
 		if(!self.dataLoadingInProgress()) {
 			self.dataLoadingInProgress(true);
@@ -104,7 +115,7 @@ function RegistrationVm() {
 			if (!!self.startDate()) {
 				requestData = { startDate: self.startDate().format("YYYY-MM-DD") }
 			}
-			
+
 			$.ajax({
 				type: 'GET',
 				url: 'get_time_entries',
@@ -118,9 +129,12 @@ function RegistrationVm() {
 				self.endDate(moment(data.endDate));
 				self.allSubmitted(self.endDate() <= self.submittedUntil());
 				for (var i = 0; i < data.projects.length; i++) {
-					self.projects.push(new Project(data.projects[i].id, data.projects[i].code, data.projects[i].name, data.projects[i].timeentries, self));
+					self.projects.push(
+						new Project(data.projects[i].id, data.projects[i].code,
+							data.projects[i].name, data.projects[i].favourite,
+							data.projects[i].timeentries, self));
 				}
-				
+
 				self.dataLoadingInProgress(false);
 			})
 			.fail(function(req, status, error){
@@ -128,32 +142,32 @@ function RegistrationVm() {
 			});
 		}
 	}
-	
+
 	self.loadNextWeek = function () {
 		if (!self.allSubmitted()) {
 			return;
 		}
 		self.startDate(self.startDate().add(7, 'days'));
 		self.endDate(self.endDate().add(7, 'days'));
-		
+
 		self.loadData();
 	}
-	
+
 	self.loadPreviousWeek = function () {
 		self.startDate(self.startDate().subtract(7, 'days'));
 		self.endDate(self.endDate().subtract(7, 'days'));
-		
+
 		self.loadData();
 	}
-	
+
 	self.submitUntilCurrentWeek = function () {
 		submitUntilCurrentWeek();
 	}
-	
+
 	function submitUntilCurrentWeek(options){
 		options = options || {};
 		options.skipFutureWarnings = options.skipFutureWarnings || false;
-		
+
 		console.log("submit with skip value", options.skipFutureWarnings);
 		$.ajax({
 			type: 'POST',
@@ -174,11 +188,11 @@ function RegistrationVm() {
 				self.hideConfirmSubmitDialog();
 			});
 	}
-	
+
 	self.submitAndHideFutureWarnings = function() {
 		submitUntilCurrentWeek({skipFutureWarnings: true});
 	}
-	
+
 	self.showConfirmSubmitDialog = function (){
 		if (self.skipConfirmSubmitDialog) {
 			submitUntilCurrentWeek({skipFutureWarnings: true});
@@ -186,7 +200,7 @@ function RegistrationVm() {
 			$("#confirmDialog").fadeIn();
 		}
 	}
-	
+
 	self.hideConfirmSubmitDialog = function (){
 		$("#confirmDialog").fadeOut();
 	}
